dc8024834ace71abb480515cdf284b57577ea7bb
Fahad <fahadfoysal16@gmail.com>
2026-01-13 14:28:59 +0600

    Signals issues fixed

--- users/signals.py
+++ users/signals.py
@@ -1,11 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from users.models import User
-from .models import VendorProfile
+from vendors.models import VendorProfile
 
 
 @receiver(post_save, sender=User)
 def create_vendor_profile(sender, instance, created, **kwargs):
     """Signal to create a VendorProfile when a new vendor user is created."""
     if created and instance.is_vendor:
-        VendorProfile.objects.create(user=instance)
+        VendorProfile.objects.create(
+            user=instance,
+            business_name=f"{instance.first_name}'s Business",
+            address=""
+            )
